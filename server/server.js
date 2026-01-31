32a8e3a14712d35334eaeb3cf0ea768aa64f974e
NaCryptoKe <NahomCryptoKetema@gmail.com>
2026-01-31 12:43:01 +0300

    fix: fixxed the trailing / on the cors origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 */
 app.use(
   cors({
-    origin:  "https://axum-arcade.vercel.app/",
+    origin:  "https://axum-arcade.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
